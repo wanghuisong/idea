d7c75a2688f6c3fc126b834b6fd2601d870fb72c
wanghuisong <1874927977@qq.con>
2018-09-11 22:34:23 +0800

    第一个工程

---
+++ dao.java
@@ -0,0 +1,19 @@
+package com.itheima;
+
+/**
+ * 作者：王辉松
+ * 邮箱：1874927977@qq.com
+ * 时间：2018/9/11 19:26
+ */
+public class dao {
+    public static void main(String[] args) {
+        System.out.println(123456789);
+        System.out.println(123456789);
+        System.out.println(123456789);
+        System.out.println(123456789);
+        System.out.println(123456789);
+        System.out.println(123456789);
+        System.out.println(123456789);
+        System.out.println(123456789);
+    }
+}
